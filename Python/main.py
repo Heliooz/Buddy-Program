d57af7b93a4876969858b9f85d1a77bf8e7a8f39
Clément <clem.maurer@hotmail.fr>
2021-11-20 15:04:31 +0100

    Python

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,3 @@
-from os import path
 import pandas as pd
 
 # Function
@@ -22,10 +21,55 @@ fast_curious_labels =  [
     'FAST&CURIOUS [Netflix Night/Welcome Night]'
 ]
 
+def fast_curious_match(data):
+    limit = data.shape[0]
+    element = data.shape[1]
+
+    match_tab = [[0] * limit for x in range(limit)]
+
+    for indexA in range(limit):
+        lineA = data.iloc[indexA]
+        for indexB in range(indexA, limit):
+            sum = 0
+            if(indexA != indexB):
+                lineB = data.iloc[indexB]
+                for index in range(element):
+                    if(lineA.iloc[index] == lineB.iloc[index]):
+                        sum+=1
+                match_tab[indexA][indexB] = sum
+                match_tab[indexB][indexA] = sum
+    return match_tab
+
+def best_match(data, match_tab):
+
+    match_dict = []
+
+    for index in range(len(match_tab)):
+        line = match_tab[index].copy()
+        sorted_line = sorted(line, reverse=True)
+        for idx in range(5):
+            max_value = sorted_line[idx]
+            max_index = line.index(max_value)
+
+            line[max_index] = 0
+
+            name1 = data.iloc[index]['First name'] + " " + data.iloc[index]['Last name']
+            name2 = data.iloc[max_index]['First name'] + " " + data.iloc[max_index]['Last name']
+
+            dict = {'couple': [name1, name2], 'value': max_value}
+
+            match_dict.append(dict)
+    return match_dict
+
 def main():
     data = pd.read_csv(path_to_src)
-    print(data.columns.values)
+    fast_curious = data[fast_curious_labels]
 
+    tab_fast_curious = fast_curious_match(fast_curious)
+    match = best_match(data, tab_fast_curious)
+    sorted_match = sorted(match, key = lambda d : d['value'],reverse=True)
+    
+    [print(val) for val in sorted_match]
 
 if __name__ == "__main__":
     main()
